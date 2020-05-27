af8945feac1444adc821cc22fd805875ca26fd58
unknown <wjdghdud118@naver.com>
2020-05-27 11:13:41 +0900

    Ch01 등록

---
+++ Ch01/src/p17/Hello.java
@@ -0,0 +1,10 @@
+package p17;
+
+public class Hello {
+
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome to the java world");
+	} //end of main
+} //end of class
---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,24 @@
+package sub1;
+/*
+ * 날짜 : 2020.04.29
+ * 내용 : 자바 기본 출력
+ */
+
+/* <기본 이해>
+ * 프로그램 -> 컴파일러 -> 기계어.
+ * 
+ * <자바의 장점>
+ * HW마다 가진 운영체제(windows, macos, unix 등)가 있으면 그들이 실행할 수 있는 유형의 파일은 정해져 있다.
+ * 하지만 JVM(java se 다운받았었지? 이게 JAVA 가상환경 패키지)을 이식하여 그들이 가진 서로 다른 유형의 실행파일을 읽어들일 수 있도록 함. 
+ * 
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) { // ctrl + space bar = 자동완성. mian 치고 ㄱㄱㄱ
+		
+		System.out.println("Hello World"); // sysout + ctrl + space bar. 기본 출력문
+		System.out.println("Hello Java");
+	}
+	
+}
